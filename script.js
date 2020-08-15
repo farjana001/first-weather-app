6017c8c355732da8a7c313a37e469e4575f67876
Farjana Islam <nipa.farzana87@gmailcon>
2020-08-15 22:42:26 +0600

    app done

---
+++ script.js
@@ -0,0 +1,48 @@
+const api = {
+    key: "869234ce2c1207539632e2b4f0cfc61d",
+    base: 'https://api.openweathermap.org/data/2.5/'
+}
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if(evt.keyCode == 13){
+        getResults(searchBox.value);
+    }
+}
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+function displayResults(weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let highLow = document.querySelector('.high-low');
+    highLow .innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`;
+
+}
+
+function dateBuilder(d) {
+    let months = ['january', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
